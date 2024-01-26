ac3a3ff44f6cfa30fe1ed6c67ac1ec4def06ec16
Bastien <bureaubastien3@gmail.com>
2024-01-26 15:40:01 +0100

    Add : Game Logic
    The game logic is working correctly with the 2 rules for a cell

---
+++ Models/Cell.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife.Models
+{
+    /// <summary>
+    /// Model that represent an instance of a cell
+    /// </summary>
+    public class Cell
+    {
+        /// <summary>
+        /// Indicate if this cell is alive
+        /// </summary>
+        public bool IsAlive { get; set; }
+
+        /// <summary>
+        /// Represent the X position of the cell
+        /// </summary>
+        public int X { get; set; }
+
+        /// <summary>
+        /// Represent the Y position of the cell
+        /// </summary>
+        public int Y { get; set; }
+
+        public Cell()
+        {
+            
+        }
+
+        public Cell(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
